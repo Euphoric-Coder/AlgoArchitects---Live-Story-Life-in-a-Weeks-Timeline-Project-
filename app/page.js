632837fbd10e88f0407bbf674a0c14bb711f353d
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-06-13 21:56:34 +0530

    Initiated Authentication System using Clerk

--- app/page.js
+++ app/page.js
@@ -1,7 +1,36 @@
+"use client";
+
+import { ModeToggle } from "@/components/theme-btn";
+import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  const { isSignedIn } = useUser();
+
+  return (
+    <div>
+      page
+      <ModeToggle />
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+          <Button asChild>
+            <SignOutButton />
+          </Button>
+        </div>
+      ) : (
+        <Button asChild>
+          <SignInButton />
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default page;
